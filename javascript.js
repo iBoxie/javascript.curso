d58d8f6e9fed85d8881fb2cdfa0128390431e02d
iBoxie <148160154+iBoxie@users.noreply.github.com>
2023-10-24 00:17:26 +0200

    First Upload
    Son las 2 primeras horas del curso.

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// Notas importantes:
+// - Javascript es sensible a mayusculas y minisculas.
+// - Respetar la sintaxis de tipo camel case. getElementById
+// - Punto y coma al final de cada sentancia.
+// Comentariode tipo linea
+/* 
+    Comentarios de varias lineas
+    (tipo bloque)
+*/
+// El codigo comentado no se ejecuta
+// alert('Este codigo no se ejecuta')
+document.write('<h2>Hola Mundo!</h2>');
+alert('Hola soy una alerta!');
+console.log('Hola desde la consola');
---
+++ operadores.js
@@ -0,0 +1,56 @@
+/* Operadires aritmeticos:
+
+        = Operadir de asignación. Se usa para asignar valores a una variable
+        + Sumar
+        - Restar
+        * Multiplicacion
+        / Division
+        % Modulo
+        ++ Aumentar
+        -- Disminuir
+
+*/
+
+const resultado = 10 + 10;
+const resultado2 = 10 - 10;
+const resultado3 = 10 % 3;
+
+let numero2 = 1;
+numero2--;
+
+
+/* Operadores de asignación:
+
+        += Suma un numero al valor de una variable.
+        -= Resta un numero al valor de una variable.
+        *= Resta un numero al valor de una variable.
+        /= Resta un numero al valor de una variable.
+        %= Obtiene el sobrante de una division y lo asigna a la variable.
+
+*/
+
+let numero = 10;
+numero +=5;
+
+/* Operadores de comparacion:
+
+        ==      Igual que
+        ===     Igual en valor y type de valor
+        !=      Diferente
+        !==     Diferente en valor y diferente en type
+        >       Mayor que
+        <       Menor que
+        >=      Mayor o igual que
+        <=      Menor o igual que
+        ?       Operador ternario
+
+*/
+
+const resultado4 = 5 > 1; //true
+const resultado5 = 20 <= 20; //true
+const resultado6 = 10 == 10; //true (solo compara el valor)
+const resultado7 = 10 === '10'; //false (comnpara valor y type)
+
+const resultado8 = 7 > 1 ? 'El primer valor es mayor que el segundo': 'El segundo valor es mayor que el primero';
+// 7 es mayor que 1?? si lo es aparece el primero texto '' sino aparece el segundo texto.
+// Sirve para poner condiciones el operador ternario
